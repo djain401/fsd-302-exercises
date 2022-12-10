0510075f25ee04c94f33e8f85b9d91efa3e46da8
Divya <djain.4444@gmail.com>
2022-12-10 11:56:07 +0000

    Adding exercises for Javascript - Saturday class/10-12-2020

---
+++ JavaScript Exercises/main.js
@@ -0,0 +1,43 @@
+//Exercise 1 - Password check
+let password = 'password1234';
+if (password.length > 7)
+{       
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+//Exercise 2 - Height test
+let height = 150;
+if(typeof(height) != "number")
+{
+    console.log("Please enter a number");
+}
+else if(height >= 150)
+{   
+    console.log("You can go the ride");
+}
+else{
+    console.log("Sorry you are not tall enough");
+}
+//Exercise 3 - Dice Prize
+let diceOne = Math.floor(Math.random()*6 + 1);
+let diceTwo = Math.floor(Math.random()*6 + 1);
+console.log(diceOne);
+console.log(diceTwo);
+if(diceOne === 6 && diceTwo === 6)
+{
+    console.log("Top prize - a bear!");
+}
+else if(diceOne === diceTwo){
+    console.log("You win three free throws!");
+}
+else if(diceOne % 2 === 0 || diceTwo % 2 === 0)
+{
+    console.log("You win one free throw!");
+}
+else{
+    console.log("You lose the game!")
+}
+
+
