b9357c82dc138864afb83d518dd815c8c9fb79d0
Divya <djain.4444@gmail.com>
2023-01-11 23:25:40 +0000

    Commiting boilerplate exercises

---
+++ boilerplate exercises/FSD 302.4 Positions Boilerplate/JavaScript exercises/main.js
@@ -0,0 +1,42 @@
+let age = 32;
+console.log(age);
+
+
+let numberOfRabbits = 2;
+numberOfRabbits = numberOfRabbits * 3;
+console.log(numberOfRabbits);
+
+let bodmasExample = 3;
+bodmasExample = (bodmasExample + 5)/6;
+console.log(bodmasExample);
+
+console.log(Math.floor(Math.random()* 6 + 1));
+
+// Exercise 1
+let myCountry = 'India';
+console.log(myCountry);
+
+//Exercise 2
+console.log(myCountry.length);
+
+
+//Exercise 3
+let favouriteFood = 'Tacos';
+console.log(favouriteFood.toUpperCase());
+
+//Exercise 4
+console.log(favouriteFood.toLowerCase());
+
+//Exercise 5
+let myName = 'Divya';
+let myAge = 32;
+message = 'Hello My name is ' + myName + ' and I am ' + myAge + ' years old. ';
+console.log(message);
+message += 'What is your name?'
+console.log(message);
+
+//Exercise 6
+let firstName = 'Divya';
+let lastName = 'Jain';
+message = `${firstName} ${lastName}`;
+console.log(message);
